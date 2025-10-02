d7c0a47c516be9a88557240bd24aac76113064b3
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2025-10-02 11:16:58 +0200

    h3 Klasse als attribuut en Rechthoek

--- src/controller/MeetkundeLauncher.java
+++ src/controller/MeetkundeLauncher.java
@@ -1,6 +1,8 @@
 package controller;
 
 import model.Cirkel;
+import model.Punt;
+import model.Rechthoek;
 
 /**
  * @author Vincent Velthuizen
@@ -13,11 +15,23 @@ public class MeetkundeLauncher {
 
         mijnCirkelArray[0] = new Cirkel(6);
         mijnCirkelArray[1] = new Cirkel();
-        mijnCirkelArray[2] = new Cirkel(3, 1, 4, "blauw");
+        mijnCirkelArray[2] = new Cirkel(3, new Punt(1, 4), "blauw");
 
         for (int cirkel = 0; cirkel < mijnCirkelArray.length; cirkel++) {
             System.out.println(mijnCirkelArray[cirkel].geefOppervlakte());
             System.out.println(mijnCirkelArray[cirkel].vertelOverGrootte());
+            System.out.println(mijnCirkelArray[cirkel].getMiddelpunt().getxCoordinaat());
+            System.out.println();
+        }
+
+        Rechthoek[] rechthoeken = new Rechthoek[3];
+
+        rechthoeken[0] = new Rechthoek();
+        rechthoeken[1] = new Rechthoek(4, 8);
+        rechthoeken[2] = new Rechthoek(10, 15, new Punt(3, -4), "rood");
+
+        for (int rechthoek = 0; rechthoek < rechthoeken.length; rechthoek++) {
+            System.out.println(rechthoeken[rechthoek].vertelOverGrootte());
             System.out.println();
         }
     }
---
+++ src/model/Punt.java
@@ -0,0 +1,26 @@
+package model;
+
+/**
+ * @author Vincent Velthuizen
+ * Een punt in de 2D ruimte
+ */
+public class Punt {
+    private static final double DEFAULT_X_COORDINAAT = 0.0;
+    private static final double DEFAULT_Y_COORDINAAT = 0.0;
+
+    private final double xCoordinaat;
+    private final double yCoordinaat;
+
+    public Punt(double xCoordinaat, double yCoordinaat) {
+        this.xCoordinaat = xCoordinaat;
+        this.yCoordinaat = yCoordinaat;
+    }
+
+    public Punt() {
+        this(DEFAULT_X_COORDINAAT, DEFAULT_Y_COORDINAAT);
+    }
+
+    public double getxCoordinaat() {
+        return xCoordinaat;
+    }
+}
