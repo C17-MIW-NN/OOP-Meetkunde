b87911af62bbd79cb2b8aa67ec3ce8ecbade5dbc
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2025-10-08 14:09:06 +0200

    h9 lezen csv bestand

--- src/controller/MeetkundeLauncher.java
+++ src/controller/MeetkundeLauncher.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -16,24 +17,31 @@ import java.util.Scanner;
 public class MeetkundeLauncher {
 
     public static void main(String[] args) {
-        ArrayList<Cirkel> cirkels = new ArrayList<>();
+        ArrayList<Rechthoek> rechthoeken = new ArrayList<>();
 
-        File stralenbestand = new File("resources/stralen.txt");
+        File rechthoekenbestand = new File("resources/rechthoeken.csv");
 
         try {
-            Scanner bestandLezer = new Scanner(stralenbestand);
+            Scanner bestandLezer = new Scanner(rechthoekenbestand);
 
-            while (bestandLezer.hasNextDouble()) {
-                double straal = bestandLezer.nextDouble();
-                cirkels.add(new Cirkel(straal));
+            while (bestandLezer.hasNextLine()) {
+                String[] rechthoekData = bestandLezer.nextLine().split(",");
+
+                double lengte = Double.parseDouble(rechthoekData[0]);
+                double breedte = Double.parseDouble(rechthoekData[1]);
+                double xCoordinaat = Double.parseDouble(rechthoekData[2]);
+                double yCoordinaat = Double.parseDouble(rechthoekData[3]);
+                String kleur = rechthoekData[4];
+
+                rechthoeken.add(new Rechthoek(lengte, breedte, new Punt(xCoordinaat, yCoordinaat), kleur));
             }
         } catch (FileNotFoundException fileNotFoundException) {
-            System.out.println("Het is niet gelukt het stralenbestand te openen");
+            System.out.println("Het is niet gelukt het rechthoekenbestand te openen");
             System.out.println(fileNotFoundException.getMessage());
         }
 
-        for (Cirkel cirkel : cirkels) {
-            System.out.println(cirkel);
+        for (Rechthoek rechthoek : rechthoeken) {
+            System.out.println(rechthoek);
             System.out.println();
         }
     }
